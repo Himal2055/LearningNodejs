ce79844dc0023434b9ceb49d796242b2cf1fd50f
Himal2055 <himalkarkee2055@gmail.com>
2023-09-06 12:26:36 +0545

    lerarn about nodemon,ejs

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express") // reruiring express package
 const app = express() // storing it in app , app vanney variable thoughout use garxau
 
-// '/' ma gayo vaney k dekhauney hae, request paxi response hunai paryo
-// app.get('/',(req,res)=>{
-//     res.send()
-// })
+//To solve problem 1
+app.set("view engine","ejs")
+
+
+    // '/' ma gayo vaney k dekhauney hae, request paxi response hunai paryo
+    // app.get('/',(req,res)=>{
+    //     res.send()
+    // })
+
+
+
+    // app.get('/',(req,res)=>{
+    //     res.send("<div><h1>This is heading part one</h1></div><p>This is paragraph</p>")
+    // })
 
 app.get('/',(req,res)=>{
+// Views vitraa ko file render garnw paya
+    res.render("home")
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("Hi himal karki")
+})
+app.get('/home',(req,res)=>{
     res.send("<h1>K xa sathi home page page</h1>")
 })
 
 app.get('/about',(req,res)=>{
-    res.send("About page")
-
+    res.render("about.ejs")
 })
+
+        // app.get('/about',(req,res)=>{
+        //     res.send("About page")
+
+        // })
 
 
 app.get("/contact",(req,res)=>{
